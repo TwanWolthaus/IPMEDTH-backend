636da3db94e79e143fbab17a0d3dfa5349aafd2c
Twan Wolthaus <twanwolthaus@gmail.com>
2024-12-02 16:59:13 +0100

    Feat: Added 3 fields to dummy

--- backend/database/migrations/2024_12_02_155519_dummy.php
+++ backend/database/migrations/2024_12_02_155519_dummy.php
@@ -11,7 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        //
+        Schema::create('dummy', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->integer('age');
+        });
     }
 
     /**
